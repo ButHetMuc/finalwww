958f23399a8481fb31ccb0e2780da7399e084deb
ButHetMuc <butle132@gmail.com>
2023-05-14 10:31:17 +0700

    s

---
+++ src/main/java/www/butle/WwwServerApplication.java
@@ -0,0 +1,13 @@
+package www.butle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WwwServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WwwServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/www/butle/WwwServerApplicationTests.java
@@ -0,0 +1,13 @@
+package www.butle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WwwServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
